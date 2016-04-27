5b5159c7d97cf82b37daedfa3ea2734b6d622ed1
Michael Zelenyuk <zell.xp@gmail.com>
2016-04-27 13:32:27 +0300

    Add Angular Recipes controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Recipe.create(name: 'Baked Potato w/ Cheese', description: 'Test description 1.', instructions: 'Test instruction 1.')
+Recipe.create(name: 'Garlic Mashed Potatoes', description: 'Test description 2.', instructions: 'Test instruction 2.')
+Recipe.create(name: 'Potatoes Au Gratin', description: 'Test description 3.', instructions: 'Test instruction 3.')
+Recipe.create(name: 'Baked Sprouts', description: 'Test description 4.', instructions: 'Test instruction 4.')
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 feature 'Looking up recipes', js: true do
+  before do
+    Recipe.create!(name: 'Baked Potato w/ Cheese', instructions: 'Test 1.')
+    Recipe.create!(name: 'Garlic Mashed Potatoes', instructions: 'Test 2.')
+    Recipe.create!(name: 'Potatoes Au Gratin', instructions: 'Test 3.')
+    Recipe.create!(name: 'Baked Brussel Sprouts', instructions: 'Test 4.')
+  end
+
   scenario 'finding recipes' do
     visit '/'
     fill_in 'keywords', with: 'baked'
